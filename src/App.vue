c3c51106912d00aafd4f5c4a342f94c83ef26c74
mihaipaulorban <mihaipaulorban@gmail.com>
2024-02-15 15:44:29 +0100

    feat: importazione e chiamata dell'api dal backend

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,26 @@
-<script>
-export default {
-  name: 'DefaultTemplate',
-};
+<script setup>
+import { onMounted, ref } from 'vue';
+import axios from 'axios';
+
+const projects = ref([]);
+
+onMounted(async () => {
+  try {
+    const response = await axios.get('http://localhost:8000/api/projects');
+    projects.value = response.data;
+    console.log(projects.value);
+  } catch (error) {
+    console.error(error);
+  }
+});
 </script>
 
-<template></template>
+<template>
+  <h1>Elenco Progetti</h1>
+  <div v-for="project in projects" :key="project.id">
+    <h2>{{ project.name }}</h2>
+    <p>{{ project.description }}</p>
+  </div>
+</template>
 
-<style scoped lang="scss">
-@use './assets/scss/partials/variables' as *;
-@use './assets/scss/partials/common ' as *;
-@use './assets/scss/partials/reset' as *;
-</style>
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue';
 import './assets/scss/style.scss';
 import App from './App.vue';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
 
 createApp(App).mount('#app');
