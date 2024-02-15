739e0384880c472cb2d08230c77407596b9701ee
mihaipaulorban <mihaipaulorban@gmail.com>
2024-02-15 15:49:05 +0100

    feat: leggero stile per la pagina

--- src/App.vue
+++ src/App.vue
@@ -16,11 +16,26 @@ onMounted(async () => {
 </script>
 
 <template>
-  <h1>Elenco Progetti</h1>
-  <div v-for="project in projects" :key="project.id">
-    <h2>{{ project.name }}</h2>
-    <p>{{ project.description }}</p>
+  <header class="bg-primary text-white text-center p-2">
+    <h1>I Miei Progetti</h1>
+  </header>
+
+  <div class="container mt-4">
+    <div class="row">
+      <div v-for="project in projects" :key="project.id" class="col-md-4 mb-4">
+        <div class="card h-100">
+          <div class="card-body">
+            <h3 class="card-title fw-bold">{{ project.title }}</h3>
+            <p class="card-text text-muted">{{ project.description }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.card {
+  height: 100%;
+}
+</style>
