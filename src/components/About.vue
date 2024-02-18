d790acc1d7eb124fd3cfe89ab8d6f5f37021f951
mihaipaulorban <mihaipaulorban@gmail.com>
2024-02-18 14:14:43 +0100

    feat: una piccola pagina about

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,3 +1,20 @@
 <template>
-  <h1>Ciao sono about</h1>
+  <div class="contact-section">
+    <h2>Contatti</h2>
+    <p>Ciao! Mi chiamo Paul e sono uno sviluppatore web di Milano.</p>
+
+    <p>
+      Puoi contattarmi via email all'indirizzo:
+      <a href="mailto:mihaipaulorban@gmail.com">mihaipaulorban@gmail.com</a>
+    </p>
+
+    <p>
+      Puoi anche trovarmi su GitHub:
+      <a href="https://github.com/mihaipaulorban"
+        >https://github.com/mihaipaulorban</a
+      >
+    </p>
+
+    <p>O chiamarmi al numero: <a href="tel:+393283654377">+393283654377</a></p>
+  </div>
 </template>
